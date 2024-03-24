ff03610a5b2aa41f55c8f519f21149b12d57f12f
Алексей <122692826+SHkipperX@users.noreply.github.com>
2024-03-24 13:03:47 +0300

    .

--- setting.py
+++ setting.py
@@ -2,17 +2,13 @@ import json
 import pprint
 
 setting = {
-    'token_2': 'vk1.a.gCpL6_y7QoMi3FXhI9mKf9Cx0iliqHI96HpcmI88OoPfQQnvfWyP6x18A17hZOoqnYtKMKULt3gMVVfBKNG8Mk8e2PxVAzb'
-               '4FVHKoGZg2h-9dnrf4SZFRXd9SVUULTj7PRkan0aHPTAJnWXHylbPjsOCXiDWTbpQZuiblbYKJ7BFMEyfl062KbOHIP9d'
-               '8RwZr0gimNO8Cd5gqFQ12mWxRw',
-    'group_id': '218947192',
-    'app_id': '6441755'
+    'token_2': '',
+    'group_id': '',
+    'app_id': ''
 }
 setting_2 = {
-    'token_2': 'vk1.a.4qYuwpKWGbccBTNuRBpvOmVPCVHHqlV6hNwW7H8BgoXko4r4pC0LKTzQI621YuH2KI63MZn8ttG9jg6g2TJuMlDajSIw3Eie5'
-             '37HOLPI1uic2JmxE1YeyYKMGSyMxnf-dZigBnX3BlRoOdCGHadFZ37ZTpSEOqrKH_fXd55L-Bl2dCVcBiWvBQ43Utyk-Z1z'
-             'OWISkYLMbvc3sLsfx2qaJg',
-    'group_id': '216846245',
-    'app_id': '6441755'
+    'token_2': '',
+    'group_id': '',
+    'app_id': ''
 }
 
